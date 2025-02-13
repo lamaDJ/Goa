12e352542f47a12c78fa4994b72efd609ea07fef
lamaDJ <andriamolashvili18@gmail.com>
2025-02-13 20:40:30 +0400

    first commit

---
+++ Level 04/Classwork/lesson.py
@@ -0,0 +1,30 @@
+string1 = 'string'
+string2 = 'stringori'
+string3 = 'stringsami'
+string4 = 'stringotxi'
+
+int1 = 1
+int2 = 2
+int3 = 3
+int4 = 4
+
+float1 = 1.5
+float2 = 2.5
+float3 = 3.5
+float4 = 4.5
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+
+print(int1)
+print(int2)
+print(int3)
+print(int4)
+
+print(float1)
+print(float2)
+print(float3)
+print(float4)
+
---
+++ Level 06/Classwork/lesson.py
@@ -0,0 +1,22 @@
+# print(361+34)
+# print(36+311)
+# print(81+4)
+# print(3+6)
+
+# print(361-34)
+# print(36-311)
+# print(81-4)
+# print(3-6)
+
+# print(361*34)
+# print(36*311)
+# print(81*4)
+# print(3*6)
+
+# print(361/34)
+# print(36/311)
+# print(81/4)
+# print(12/6)
+
+parents_age = int(input('თქვენი მშობლების ასაკი:'))
+print('თქვენი მშობლების ასაკი ოც წელში:',parents_age + 20)
